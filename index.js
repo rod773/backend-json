e7454966946e22b5ed9f85b49aaa431023d24904
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-02 20:28:41 +0200

    index js

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(5000, () => {
+  console.log("http://localhost:5000");
+});
