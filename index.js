ad1d62092cadb09bdc215c31d08aef5ad3c3156d
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-03 19:26:35 +0200

    read write data json

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 import express from "express";
+import fs from "fs";
+
+let obj = [];
+
+fs.readFile("data.json", "utf8", function (err, data) {
+  // Display the file content
+  console.log(data);
+  obj = JSON.parse(data);
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ ok: true });
+  res.json(obj);
+});
+
+const newData = [
+  {
+    id: 1,
+    title: "bird",
+  },
+  {
+    id: 2,
+    title: "eagle",
+  },
+];
+
+app.get("/products", (req, res) => {
+  fs.writeFile("data.json", JSON.stringify(newData), (err) => {
+    if (err) throw error;
+    console.log("done writting");
+  });
+  fs.readFile("data.json", "utf8", function (err, data) {
+    // Display the file content
+    console.log(data);
+    obj = JSON.parse(data);
+  });
+  res.json(obj);
 });
 
 app.listen(5000, () => {
