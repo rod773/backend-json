08213d71d97d5b07d5d38f191357d1d66dbdb768
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-04 14:41:13 +0200

    validate email

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,12 @@
+import { validationResult } from "express-validator";
+
 export const register = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   console.log(req.body);
   res.json(req.body);
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { login, register } from "./../controllers/auth.controller.js";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post(
+  "/register",
+  [body("email", "formato email incorrecto").isEmail().normalizeEmail()],
+  register
+);
 
 router.post("/login", login);
 
